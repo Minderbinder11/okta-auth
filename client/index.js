7120be516de47fd2e673ea1c4a36a08093797274
Phil Barow <pbarow@gmail.com>
2017-05-15 06:43:34 -0700

    saving for synch with origin

--- client/index.js
+++ client/index.js
@@ -17,8 +17,16 @@ button.addEventListener('click', () => {
       console.log(err);
     });      
 
-// CURL REQUEST - I need to update the session token before making this request, this is aall to sessions, I want a call to auth    
-//curl -v POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"sessionToken": "20111BTAQMZ4QghQyrdXJ3wIziPVYMd9Bhq0ECntRYCxfnCmeZeAD4x"}' "https://dev-477147.oktapreview.com/api/v1/sessions"
+/*
+ * api/vi/sessions
+ * this will take a sessionToken  and retrun a sessionsCookie
+ * sessionsCookie provide access to your Okta organization and applications across 
+ * web requests for an interactive user agent such as a web browser.
+ */
+
+// CURL REQUEST - I need to update the session token before making this request, 
+// this is aall to sessions, I want a call to auth    
+// curl -v POST -H "Accept: application/json" -H "Content-Type: application/json" -d '{"sessionToken": "20111BTAQMZ4QghQyrdXJ3wIziPVYMd9Bhq0ECntRYCxfnCmeZeAD4x"}' "https://dev-477147.oktapreview.com/api/v1/sessions"
 
 });
 
